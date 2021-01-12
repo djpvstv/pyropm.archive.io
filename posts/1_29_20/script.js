a56717a58f6ced738ecec4c4fcdc463c4fc5b3a9
PyroPM <sean.k.mapes@gmail.com>
2021-01-11 20:56:22 -0500

    huh okay

--- posts/1_29_20/script.js
+++ posts/1_29_20/script.js
@@ -3,7 +3,7 @@ function open_json() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            return JSON.parse(xhttp);
+            return JSON.parse(this.textcontent);
         }
     };
     xhttp.open("GET", "filename", true);
