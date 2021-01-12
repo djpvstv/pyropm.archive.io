a62ce7e3a0595e867a4fbe99103ab14cb14279b5
PyroPM <sean.k.mapes@gmail.com>
2021-01-11 21:01:51 -0500

    okay this is dumb

--- posts/1_29_20/script.js
+++ posts/1_29_20/script.js
@@ -3,7 +3,7 @@ function open_json() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            return this;
+            return this.responseText;
         }
     };
     xhttp.open("GET", "filename", true);
